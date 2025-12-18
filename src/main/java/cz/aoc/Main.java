542007a11aa28d763fb20fa711da2a543ba5acf6
Jan Skořepa <xskorep1@fi.muni.cz>
2025-12-18 17:35:15 +0100

    small debug changes

--- src/main/java/cz/aoc/Main.java
+++ src/main/java/cz/aoc/Main.java
@@ -1,9 +1,11 @@
 package cz.aoc;
 
 import cz.aoc.day01.Day01;
+import cz.aoc.day02.Day02;
 
 public class Main {
     public static void main(String[] args) {
         Day01.main(args);
+        Day02.main(args);
     }
 }
