2da244d2e735cad47b441400b642a91cc9c0352b
Jan Skořepa <skorepa-jan@seznam.cz>
2025-12-19 12:56:14 +0100

    day03-task2 solved

--- src/main/java/cz/aoc/day03/Day03.java
+++ src/main/java/cz/aoc/day03/Day03.java
@@ -9,12 +9,21 @@ public class Day03 {
         List<String> banks = readLines("day03");
 
         System.out.println("First task - total output joltage: " + solveFirstPuzzle(banks));
+        System.out.println("Second task - total output joltage: " + solveSecondPuzzle(banks));
     }
 
     public static int solveFirstPuzzle(List<String> banks) {
         int sum = 0;
         for (String batteries : banks) {
             sum += findBiggestNum(batteries);
+        }
+        return sum;
+    }
+
+    public static long solveSecondPuzzle(List<String> banks) {
+        long sum = 0;
+        for (String batteries : banks) {
+            sum += findTwelveNum(batteries);
         }
         return sum;
     }
@@ -39,6 +48,38 @@ public class Day03 {
             if (max < currNum) max = currNum;
         }
         return max;
+    }
+
+    public static Long findTwelveNum(String batteries) {
+        if (batteries.length() == 12) return Long.parseLong(batteries);
+
+        return findTwelveNumRecursive(batteries, 0, 12);
+    }
+
+    private static long findTwelveNumRecursive(String batteries, int startIndex, int digitsNeeded) {
+        if (digitsNeeded == 0) {
+            return 0;
+        }
+
+        int remainingStringLength = batteries.length() - startIndex;
+        int maxSkip = remainingStringLength - digitsNeeded;
+        int searchEndIndex = startIndex + maxSkip;
+
+        int maxDigit = -1;
+        int index = -1;
+        for (int i = startIndex; i <= searchEndIndex; i++) {
+            int current = Integer.parseInt(String.valueOf(batteries.charAt(i)));
+            if (current > maxDigit) {
+                maxDigit = current;
+                index = i;
+            }
+            if (maxDigit == 9) {
+                break;
+            }
+        }
+
+        long positionVal = (long) Math.pow(10, digitsNeeded - 1);
+        return (maxDigit * positionVal) + findTwelveNumRecursive(batteries, index + 1, digitsNeeded - 1);
     }
 
 }
--- src/test/java/cz/aoc/day03/Day03Test.java
+++ src/test/java/cz/aoc/day03/Day03Test.java
@@ -4,6 +4,7 @@ package cz.aoc.day03;
 import org.junit.jupiter.api.Test;
 
 import static cz.aoc.day03.Day03.findBiggestNum;
+import static cz.aoc.day03.Day03.findTwelveNum;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -15,5 +16,13 @@ public class Day03Test {
         assertEquals(89, findBiggestNum("811111111111119"));
         assertEquals(78, findBiggestNum("234234234234278"));
         assertEquals(92, findBiggestNum("818181911112111"));
+    }
+
+    @Test
+    void testFindTwelveNum() {
+        assertEquals(987654321111L, findTwelveNum("987654321111111"));
+        assertEquals(811111111119L, findTwelveNum("811111111111119"));
+        assertEquals(434234234278L, findTwelveNum("234234234234278"));
+        assertEquals(888911112111L, findTwelveNum("818181911112111"));
     }
 }
