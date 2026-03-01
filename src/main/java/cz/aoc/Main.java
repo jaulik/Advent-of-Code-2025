e5c5a6a7051fa09172020f48b9e644d043b4b04d
Jan Skořepa <skorepa-jan@seznam.cz>
2026-03-01 13:02:46 +0100

    Update Main class to run solutions for days 3-9

--- src/main/java/cz/aoc/Main.java
+++ src/main/java/cz/aoc/Main.java
@@ -2,10 +2,25 @@ package cz.aoc;
 
 import cz.aoc.day01.Day01;
 import cz.aoc.day02.Day02;
+import cz.aoc.day03.Day03;
+import cz.aoc.day04.Day04;
+import cz.aoc.day05.Day05;
+import cz.aoc.day06.Day06;
+import cz.aoc.day07.Day07;
+import cz.aoc.day08.Day08;
+import cz.aoc.day09.Day09;
 
 public class Main {
     public static void main(String[] args) {
         Day01.main(args);
         Day02.main(args);
+        Day03.main(args);
+        Day04.main(args);
+        Day05.main(args);
+        Day06.main(args);
+        Day07.main(args);
+        Day08.main(args);
+        Day08.main(args);
+        Day09.main(args);
     }
 }
