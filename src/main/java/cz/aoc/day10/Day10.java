f81546154305e0b46de2e9ff79081fb0f220ba9c
Jan Skořepa <skorepa-jan@seznam.cz>
2026-03-10 22:52:03 +0100

    rename methods

--- src/main/java/cz/aoc/day10/Day10.java
+++ src/main/java/cz/aoc/day10/Day10.java
@@ -18,7 +18,7 @@ public class Day10 {
 
     public static int solveFirstPuzzle(List<String> lines) {
         return lines.stream()
-                .mapToInt(Day10::processLine)
+                .mapToInt(Day10::solveMachineLights)
                 .filter(value -> value >= 0)
                 .sum();
     }
@@ -30,12 +30,12 @@ public class Day10 {
      * @return The fewest button presses required to reach the target lights configuration,
      * or -1 if no path is found.
      */
-    private static int processLine(String line) {
+    private static int solveMachineLights(String line) {
         String[] parts = line.split(" ");
         String lights = parts[0].substring(1, parts[0].length() - 1);
-        int targetLights = getTargetLights(lights);
-        List<Integer> buttons = getButtons(parts);
-        return countButtonPresses(buttons, targetLights, lights.length());
+        int targetLights = parseTargetLightMask(lights);
+        List<Integer> buttons = parseButtonToggleMasks(parts);
+        return bfsToggleLights(buttons, targetLights, lights.length());
     }
     /**
      * Uses BFS to find the shortest path from all lights off to the target configuration.
@@ -46,7 +46,7 @@ public class Day10 {
      * @return The fewest button presses required to reach the target lights configuration,
      * or -1 if no path is found.
      */
-    private static int countButtonPresses(List<Integer> buttons, int targetLights, int numLights) {
+    private static int bfsToggleLights(List<Integer> buttons, int targetLights, int numLights) {
         Queue<int[]> buttonsQueue = new LinkedList<>();
         // Array to keep track of visited states to avoid infinite loops.
         // 1 << numLights equals 2^numLights, covering all possible on/off combinations.
@@ -71,7 +71,7 @@ public class Day10 {
         return -1;
     }
 
-    private static int getTargetLights(String lights) {
+    private static int parseTargetLightMask(String lights) {
         int numLights = lights.length();
         int targetLights = 0;
         for (int i = 0; i < numLights; i++) {
@@ -83,7 +83,7 @@ public class Day10 {
         return targetLights;
     }
 
-    private static List<Integer> getButtons(String[] lineParts) {
+    private static List<Integer> parseButtonToggleMasks(String[] lineParts) {
         List<Integer> buttons = new ArrayList<>();
         for (int i = 1; i < lineParts.length - 1; i++) {
             String nums = lineParts[i].substring(1, lineParts[i].length() - 1);
